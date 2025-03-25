9539a57120940a538e4e1bd2742de64e6e57a7bf
Fikireyesusb <155649808+Fikireyesusb@users.noreply.github.com>
2025-03-25 16:38:20 +0100

    Delete flower.js

--- flower.js
+++
@@ -1,8 +0,0 @@
-function openOrderForm(flowerType) {
-    document.getElementById('order-modal').style.display = 'flex';
-    document.getElementById('flower').value = flowerType;
-}
-
-function closeOrderForm() {
-    document.getElementById('order-modal').style.display = 'none';
-}
